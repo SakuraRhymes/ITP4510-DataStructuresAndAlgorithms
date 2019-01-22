c8aae6d758d6eed974bc62afa2e92ac06a3ad405
SakuraRhymes <lovemyway0224@gmail.com>
2019-01-22 09:30:29 +0800

    Added Lab1.1

---
+++ src/Lab01/Lab1_1/Worker.java
@@ -0,0 +1,19 @@
+package Lab01.Lab1_1;
+
+public class Worker {
+    private String name;
+    protected double salary;
+
+    public Worker(String name) {
+        this.name = name;
+    }
+
+    public double earnings() {
+        return salary;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
+
