023c81e82dfabd56cd4795d897376cfcd52ba2d3
SakuraRhymes <lovemyway0224@gmail.com>
2019-01-22 09:52:21 +0800

    Added Lab1.2

---
+++ src/Lab01/Lab1_2/Drink.java
@@ -0,0 +1,22 @@
+package Lab01.Lab1_2;
+
+public class Drink extends Food {
+    protected int volume;
+
+    public Drink(String name, int price, int volume) {
+        super(name, price);
+        this.volume = volume;
+    }
+
+    public void setPrice(int price) {
+        if (price < 5)
+            this.price = 5;
+        else
+            this.price = price;
+    }
+
+    public String toString() {
+        return super.toString() + ", volume=" + volume;
+    }
+}
+
